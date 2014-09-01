4c7ddb7720a00511930fee41d066298c671f2949
kgryte <kgryte@gmail.com>
2014-08-31 18:45:23 -0700

    [UPDATE] keywords.

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,19 @@
-var module = require( './../lib' );
+var isLength = require( './../lib' );
+
+console.log( isLength( 'beep', 4 ) );
+// Returns true
+
+console.log( isLength( 'foo', 2, 10 ) );
+// Returns true
+
+console.log( isLength( [1,2,3], 3 ) );
+// Returns true
+
+console.log( isLength( [4,5,6], 0, 5 ) );
+// Returns true
+
+console.log( isLength( 'baz', 4 ) );
+// Returns false
+
+console.log( isLength( [1,2], 10, 12 ) );
+// Returns false
